f810cdb90ba69bf8f892eb055326aa8df7717197
jcheahh <jackcheah2@gmail.com>
2021-04-07 23:58:26 +0800

    Update App.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-} from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Container from "react-bootstrap/Container";
 import Home from "./component/Home";
-import Product from "./component/Product";
-
+// import Product from "./component/Product";
 
 export default function App() {
     return (
         <Container>
             <Router>
                 <Switch>
-                    <Route exact path="/product/:id">
+                    {/* <Route exact path="/product/:id">
                         <Product />
-                    </Route>
+                    </Route> */}
                     <Route exact path="/users">
                         <Users />
                     </Route>
-                    <Route exact path="/">
+                    <Route path="/">
                         <Home />
                     </Route>
                 </Switch>
@@ -29,8 +25,6 @@ export default function App() {
     );
 }
 
-
 function Users() {
     return <h2>Users</h2>;
 }
-
