efe645a985d9525fe744132908988a50d7e54bcc
jcheahh <jackcheah2@gmail.com>
2021-04-08 00:01:12 +0800

    Add state for modal and styling the Home page

--- src/component/Home.tsx
+++ src/component/Home.tsx
@@ -1,37 +1,63 @@
-import { Button, Card, Carousel, Dropdown, DropdownButton, Nav, Navbar, Row } from "react-bootstrap";
+import React from "react";
+import {
+    Button,
+    Card,
+    CardDeck,
+    Carousel,
+    Dropdown,
+    DropdownButton,
+    Nav,
+    Navbar,
+    NavDropdown,
+} from "react-bootstrap";
+import { Route } from "react-router";
+import { Link } from "react-router-dom";
+import CardModal from "./CardModal";
+import Product from "./Product";
 
 export default function Home() {
+    const [modalShow, setModalShow] = React.useState(false);
+    const [cardModalShow, setCardModalShow] = React.useState(false);
+
     return (
         <>
-            <Navbar bg="light">
+            <Navbar collapseOnSelect expand="lg" bg="light">
                 <Navbar.Brand href="/">Home</Navbar.Brand>
-                <Nav className="mr-auto">
-                    <Nav.Link href="/#876">Menu</Nav.Link>
-                    <Nav.Link href="/stores">Store Information</Nav.Link>
-                    <DropdownButton variant="warning" id="dropdown-basic-button" title="English">
-                        <Dropdown.Item href="/">English</Dropdown.Item>
-                        <Dropdown.Item href="/">Bahasa</Dropdown.Item>
-                        <Dropdown.Item href="/">中文</Dropdown.Item>
-                    </DropdownButton>
-                    <Nav.Link href="/cart">Cart</Nav.Link>
-                </Nav>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="mr-auto" />
+                    <Nav>
+                        <Nav.Link href="/#876">Menu</Nav.Link>
+                        <Nav.Link href="/stores">Store Information</Nav.Link>
+                        <DropdownButton
+                            variant="default"
+                            id="dropdown-basic-button"
+                            title="English"
+                        >
+                            <Dropdown.Item href="/">English</Dropdown.Item>
+                            <Dropdown.Item href="/">Bahasa</Dropdown.Item>
+                            <Dropdown.Item href="/">中文</Dropdown.Item>
+                        </DropdownButton>
+                        <Nav.Link href="/cart">Cart</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
             </Navbar>
             <Carousel>
-                <Carousel.Item interval={1000}>
+                <Carousel.Item interval={3000}>
                     <img
                         className="d-block w-100"
                         src="https://ucarecdn.com/c82fbc1e-4ae0-43d4-a210-3b1d2c182f6d/-/crop/1235x514/48,48/-/format/auto/-/resize/1024x/"
                         alt="First slide"
                     />
                 </Carousel.Item>
-                <Carousel.Item interval={1000}>
+                <Carousel.Item interval={3000}>
                     <img
                         className="d-block w-100"
                         src="https://ucarecdn.com/e8b2cf63-d45d-4691-a9ab-590c776d46d5/-/crop/1291x538/0,0/-/format/auto/-/resize/1024x/"
                         alt="Second slide"
                     />
                 </Carousel.Item>
-                <Carousel.Item>
+                <Carousel.Item interval={3000}>
                     <img
                         className="d-block w-100"
                         src="https://ucarecdn.com/4f4cf856-1e47-4e7e-a038-157e4485060f/-/crop/1291x537/0,21/-/format/auto/-/resize/1024x/"
@@ -39,98 +65,251 @@ export default function Home() {
                     />
                 </Carousel.Item>
             </Carousel>
-            <div>
-                <a href="/#876">
-                    <div>
-                        <h4>【Super Deal Limited Time Promo】 限时优惠</h4>
-                    </div>
-                </a>
-                <a href="/#877">
-                    <div>
-                        <h4>【Kitchen Must Have】厨神必备 *Delivery Tuesday to Sunday 2pm - 6pm</h4>
-                    </div>
-                </a>
-            </div>
+            <br />
+            <h2 className="Text1 text-center text-warning">
+                Don't Miss the Promotion
+            </h2>
+            <br />
 
+            <Card style={{ width: "20rem" }} className="card">
+                <Link to="/" onClick={() => setCardModalShow(true)}>
+                    <div className="cardHeader text-white">
+                        <b>
+                            <p className="Text3">RM15.00</p>
+                        </b>
+                        <p>Off Total Order!</p>
+                    </div>
+                    <div className="cardBody">
+                        <p>
+                            Code " SF15 " to get RM15 OFF when spend RM250 &
+                            above
+                        </p>
+                        <p className="details">View Details</p>
+                    </div>
+                </Link>
+            </Card>
+
+            <br />
+            <Nav
+                fill
+                variant="tabs"
+                defaultActiveKey="/"
+                className="shadow p-3 mb-5 bg-white rounded"
+            >
+                <Nav.Item>
+                    <Nav.Link href="/#876">
+                        【Super Deal Limited Time Promo】 限时优惠
+                    </Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link href="/#877">
+                        【Kitchen Must Have】厨神必备 *Delivery Tuesday to
+                        Sunday 2pm - 6pm
+                    </Nav.Link>
+                </Nav.Item>
+                <NavDropdown title="More" id="nav-dropdown">
+                    <NavDropdown.Item href="/#876">
+                        【Super Deal Limited Time Promo】 限时优惠
+                    </NavDropdown.Item>
+                    <NavDropdown.Item href="/#877">
+                        【Kitchen Must Have】厨神必备 *Delivery Tuesday to
+                        Sunday 2pm - 6pm
+                    </NavDropdown.Item>
+                </NavDropdown>
+            </Nav>
+            <br />
             <div>
-                <div id="876" ></div>
-                <div>
+                <div id="876"></div>
+                <div className="Text1 text-warning">
                     【Super Deal Limited Time Promo】 限时优惠
                 </div>
             </div>
-
-            <p>限时优惠！好康多多~</p>
-            <Row>
-                <Card style={{ width: '17.5rem' }}>
-                    <a href="/product/8861369">
-                        <Card.Img variant="top" src="https://images.weserv.nl/?url=https://s3-ap-southeast-1.amazonaws.com/v3-live.image.oddle.me/product/5Combo5ChickenChopReadytoCook28b42f.jpg&amp;w=500&amp;h=500&amp;fit=cover" />
+            <br />
+            <p className="Text2 text-warning">限时优惠！好康多多~</p>
+            <CardDeck>
+                <Card
+                    style={{ width: "17rem" }}
+                    className="shadow bg-white rounded"
+                >
+                    <Link
+                        to="/product/8861369"
+                        onClick={() => setModalShow(true)}
+                    >
+                        <Card.Img
+                            variant="top"
+                            src="https://images.weserv.nl/?url=https://s3-ap-southeast-1.amazonaws.com/v3-live.image.oddle.me/product/5Combo5ChickenChopReadytoCook28b42f.jpg&amp;w=500&amp;h=500&amp;fit=cover"
+                        />
                         <Card.Body>
-                            <Card.Title>5包装鸡扒 Combo 5 Chicken Chop - Ready to Cook</Card.Title>
-                            <Card.Text>
-                                RM59.90
-                        </Card.Text>
-                            <Button variant="warning">Add</Button>
+                            <Card.Title>
+                                5包装鸡扒 Combo 5 Chicken Chop - Ready to Cook
+                            </Card.Title>
+                            <Card.Text>RM59.90</Card.Text>
+                            <div className="text-center">
+                                <Button
+                                    variant="warning"
+                                    className="text-white"
+                                    block
+                                >
+                                    Add
+                                </Button>
+                            </div>
                         </Card.Body>
-                    </a>
+                    </Link>
                 </Card>
-                <Card style={{ width: '17.5rem' }}>
-                    <a href="/product/8861370">
-                        <Card.Img variant="top" src="https://images.weserv.nl/?url=https://s3-ap-southeast-1.amazonaws.com/v3-live.image.oddle.me/product/V21Pumpkinslice800gfb20c2.jpg&w=500&h=500&fit=cover" />
+                <Card
+                    style={{ width: "17rem" }}
+                    className="shadow bg-white rounded"
+                >
+                    <Link
+                        to="/product/8861370"
+                        onClick={() => setModalShow(true)}
+                    >
+                        <Card.Img
+                            variant="top"
+                            src="https://images.weserv.nl/?url=https://s3-ap-southeast-1.amazonaws.com/v3-live.image.oddle.me/product/V21Pumpkinslice800gfb20c2.jpg&w=500&h=500&fit=cover"
+                        />
                         <Card.Body>
-                            <Card.Title>V21  南瓜块 Pumpkin slice (800g+-)</Card.Title>
-                            <Card.Text>
-                                RM3.90
-                        </Card.Text>
-                            <Button variant="warning">Add</Button>
+                            <Card.Title>
+                                V21 南瓜块 Pumpkin slice (800g+-)
+                            </Card.Title>
+                            <br />
+                            <Card.Text>RM3.90</Card.Text>
+                            <div className="text-center">
+                                <Button
+                                    variant="warning"
+                                    className="text-white"
+                                    block
+                                >
+                                    Add
+                                </Button>
+                            </div>
                         </Card.Body>
-                    </a>
+                    </Link>
                 </Card>
-                <Card style={{ width: '17.5rem' }}>
-                    <a href="/product/8861371">
-                        <Card.Img variant="top" src="https://images.weserv.nl/?url=https://s3-ap-southeast-1.amazonaws.com/v3-live.image.oddle.me/product/V16LimaBeans220ged9a6c.jpg&w=500&h=500&fit=cover" />
+                <Card
+                    style={{ width: "17rem" }}
+                    className="shadow bg-white rounded"
+                >
+                    <Link
+                        to="/product/8861371"
+                        onClick={() => setModalShow(true)}
+                    >
+                        <Card.Img
+                            variant="top"
+                            src="https://images.weserv.nl/?url=https://s3-ap-southeast-1.amazonaws.com/v3-live.image.oddle.me/product/V16LimaBeans220ged9a6c.jpg&w=500&h=500&fit=cover"
+                        />
                         <Card.Body>
-                            <Card.Title>V16  棉豆 Lima Beans (220g+-)</Card.Title>
-                            <Card.Text>
-                                RM1.90
-                        </Card.Text>
-                            <Button variant="warning">Add</Button>
+                            <Card.Title>
+                                V16 棉豆 Lima Beans (220g+-)
+                            </Card.Title>
+                            <br />
+                            <Card.Text>RM1.90</Card.Text>
+                            <div className="text-center">
+                                <Button
+                                    variant="warning"
+                                    className="text-white"
+                                    block
+                                >
+                                    Add
+                                </Button>
+                            </div>
                         </Card.Body>
-                    </a>
+                    </Link>
                 </Card>
-                <Card style={{ width: '17.5rem' }}>
-                    <a href="/product/8861372">
-                        <Card.Img variant="top" src="https://images.weserv.nl/?url=https://s3-ap-southeast-1.amazonaws.com/v3-live.image.oddle.me/product/V1FrenchBeanse929df.jpg&w=500&h=500&fit=cover" />
+                <Card
+                    style={{ width: "17rem" }}
+                    className="shadow bg-white rounded"
+                >
+                    <Link
+                        to="/product/8861372"
+                        onClick={() => setModalShow(true)}
+                    >
+                        <Card.Img
+                            variant="top"
+                            src="https://images.weserv.nl/?url=https://s3-ap-southeast-1.amazonaws.com/v3-live.image.oddle.me/product/V1FrenchBeanse929df.jpg&w=500&h=500&fit=cover"
+                        />
                         <Card.Body>
-                            <Card.Title>V1   富贵豆 French Beans (250g+-)</Card.Title>
-                            <Card.Text>
-                                RM3.90
-                        </Card.Text>
-                            <Button variant="warning">Add</Button>
+                            <Card.Title>
+                                V1 富贵豆 French Beans (250g+-)
+                            </Card.Title>
+                            <br />
+                            <Card.Text>RM3.90</Card.Text>
+                            <div className="text-center ">
+                                <Button
+                                    variant="warning"
+                                    className="text-white"
+                                    block
+                                >
+                                    Add
+                                </Button>
+                            </div>
                         </Card.Body>
-                    </a>
+                    </Link>
                 </Card>
-
-            </Row>
+            </CardDeck>
+            <br />
             <div>
                 <div id="877"></div>
-                <div>【Kitchen Must Have】厨神必备 *Delivery Tuesday to Sunday 2pm - 6pm</div>
+                <div className="Text1 text-warning">
+                    【Kitchen Must Have】厨神必备 *Delivery Tuesday to Sunday
+                    2pm - 6pm
+                </div>
             </div>
-            <p>精心挑选的烹饪好帮手；让厨房小白也能轻松变厨神！</p>
+            <br />
+            <p className="Text2 text-warning">
+                精心挑选的烹饪好帮手；让厨房小白也能轻松变厨神！
+            </p>
 
-            <p> ** KLANG, Shah Alam，Cyberjaya地区，每周三运送！ </p>
+            <p className="Text2 text-warning">
+                {" "}
+                ** KLANG, Shah Alam，Cyberjaya地区，每周三运送！{" "}
+            </p>
 
-            <Card style={{ width: '17.5rem' }}>
-                <a href="/product/8861373">
-                    <Card.Img variant="top" src="https://images.weserv.nl/?url=https://s3-ap-southeast-1.amazonaws.com/v3-live.image.oddle.me/product/Sinceromultifunction6LpressurecookerSPC9001baee74.jpg&w=500&h=500&fit=cover" />
+            <Card
+                style={{ width: "17rem" }}
+                className="shadow bg-white rounded"
+            >
+                <Link to="/product/8861373" onClick={() => setModalShow(true)}>
+                    <Card.Img
+                        variant="top"
+                        src="https://images.weserv.nl/?url=https://s3-ap-southeast-1.amazonaws.com/v3-live.image.oddle.me/product/Sinceromultifunction6LpressurecookerSPC9001baee74.jpg&w=500&h=500&fit=cover"
+                    />
                     <Card.Body>
-                        <Card.Title>Sincero multifunction 6L pressure cooker SPC-9001 多功能的智能电压力锅</Card.Title>
-                        <Card.Text>
-                            RM469.90
-                        </Card.Text>
-                        <Button variant="warning">Add</Button>
+                        <Card.Title>
+                            Sincero multifunction 6L pressure cooker SPC-9001
+                            多功能的智能电压力锅
+                        </Card.Title>
+                        <Card.Text>RM469.90</Card.Text>
+                        <div className="text-center">
+                            <Button
+                                variant="warning"
+                                className="text-white"
+                                block
+                            >
+                                Add
+                            </Button>
+                        </div>
                     </Card.Body>
-                </a>
+                </Link>
             </Card>
+            <br />
+
+            <Route
+                path="/product/:id"
+                render={() => {
+                    return (
+                        <Product
+                            show={modalShow}
+                            onHide={() => setModalShow(false)}
+                        />
+                    );
+                }}
+            />
+
+            <CardModal
+                show={cardModalShow}
+                onHide={() => setCardModalShow(false)}
+            />
         </>
     );
 }
