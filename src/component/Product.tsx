d8b3dd647ae281013220606fb52f82c30730695c
jcheahh <jackcheah2@gmail.com>
2021-04-07 23:59:48 +0800

    Update Product page using Modal

--- src/component/Product.tsx
+++ src/component/Product.tsx
@@ -1,28 +1,66 @@
-import { useParams } from "react-router";
-import products from "../products"
+import { Col, Figure, Modal, Row } from "react-bootstrap";
+import { useHistory, useParams } from "react-router";
+import products from "../products";
 
 interface RouteParams {
     id: string;
 }
 
-export default function Product() {
-    const { id } = useParams<RouteParams>();
-    const product = products.find(p => p.id === id)
-    if (!product) {
-        return (
-            <div>Product not found</div>
-        )
-    }
-    return (
-        <>
-            <button>close</button>
-            <div>
-                {product.photoUrl.map((photo) => <img src={photo.url} alt={photo.altText} />)}
-                {product.title}
-                {product.description}
-                {product.price}
-            </div>
-        </>
-    )
+interface ProductModalProps {
+    show: boolean;
+    onHide: () => void;
 }
 
+export default function Product(props: ProductModalProps) {
+    const { id } = useParams<RouteParams>();
+    const history = useHistory();
+
+    const { from } = { from: { pathname: "/" } };
+
+    const product = products.find((p) => p.id === id);
+    if (!product) {
+        return <div>Product not found</div>;
+    }
+    return (
+        <Modal
+            {...props}
+            size="lg"
+            aria-labelledby="contained-modal-title-vcenter"
+            centered
+        >
+            <button
+                type="button"
+                className="close"
+                aria-label="Close"
+                onClick={() => {
+                    history.replace(from);
+                }}
+            >
+                <span aria-hidden="true">&times;</span>
+            </button>
+            <Row>
+                <Col>
+                    {product.photoUrl.map((photo) => (
+                        <Figure.Image
+                            width={471}
+                            height={480}
+                            alt={photo.altText}
+                            src={photo.url}
+                        />
+                    ))}
+                </Col>
+                <Col>
+                    <h3>{product.title}</h3>
+                    <p
+                        style={{
+                            whiteSpace: "break-spaces",
+                        }}
+                    >
+                        {product.description}
+                    </p>
+                    <div>{product.price}</div>
+                </Col>
+            </Row>
+        </Modal>
+    );
+}
