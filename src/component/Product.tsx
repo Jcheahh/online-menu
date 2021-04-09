bde7751b6c095d52c90460eefc8a6c91b8fec327
jcheahh <jackcheah2@gmail.com>
2021-04-09 18:00:41 +0800

    Update product

--- src/component/Product.tsx
+++ src/component/Product.tsx
@@ -1,6 +1,7 @@
-import { Col, Figure, Modal, Row } from "react-bootstrap";
+import { Button, Col, Figure, Modal, Row } from "react-bootstrap";
 import { useHistory, useParams } from "react-router";
 import products from "../products";
+import "../css/Product.css";
 
 interface RouteParams {
     id: string;
@@ -28,39 +29,45 @@ export default function Product(props: ProductModalProps) {
             aria-labelledby="contained-modal-title-vcenter"
             centered
         >
-            <button
-                type="button"
-                className="close"
-                aria-label="Close"
-                onClick={() => {
-                    history.replace(from);
-                }}
-            >
-                <span aria-hidden="true">&times;</span>
-            </button>
-            <Row>
-                <Col>
-                    {product.photoUrl.map((photo) => (
-                        <Figure.Image
-                            width={471}
-                            height={480}
-                            alt={photo.altText}
-                            src={photo.url}
-                        />
-                    ))}
-                </Col>
-                <Col>
-                    <h3>{product.title}</h3>
-                    <p
-                        style={{
-                            whiteSpace: "break-spaces",
-                        }}
-                    >
-                        {product.description}
-                    </p>
-                    <div>{product.price}</div>
-                </Col>
-            </Row>
+            <div className="productModal">
+                <button
+                    type="button"
+                    className="close"
+                    aria-label="Close"
+                    onClick={() => {
+                        history.replace(from);
+                    }}
+                >
+                    <span aria-hidden="true">&times;</span>
+                </button>
+                <Row>
+                    <Col>
+                        {product.photoUrl.map((photo) => (
+                            <Figure.Image
+                                width={471}
+                                height={480}
+                                alt={photo.altText}
+                                src={photo.url}
+                            />
+                        ))}
+                    </Col>
+                    <Col>
+                        <h3>{product.title}</h3>
+                        <p
+                            style={{
+                                whiteSpace: "break-spaces",
+                            }}
+                        >
+                            {product.description}
+                        </p>
+                        <div>{product.price}</div>
+                    </Col>
+                </Row>
+                <br />
+                <Button variant="warning" className="productModalButton" block>
+                    Add
+                </Button>
+            </div>
         </Modal>
     );
 }
