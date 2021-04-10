14197600119415ef1bc3363c7ed07736a78ff139
jcheahh <jackcheah2@gmail.com>
2021-04-10 13:33:56 +0800

    Change photo array to photo

--- src/products.ts
+++ src/products.ts
@@ -1,25 +1,25 @@
 interface Photo {
     url: string;
-    altText: string
+    altText: string;
 }
 
 interface Product {
     id: string;
-    photoUrl: Photo[];
+    photo: Photo;
     title: string;
     description: string;
-    price: string;
+    price: number;
 }
 
 const products: Product[] = [
     {
         id: "8861369",
-        photoUrl: [
-            {
-                url: "https://images.weserv.nl/?url=https://s3-ap-southeast-1.amazonaws.com/v3-live.image.oddle.me/product/5Combo5ChickenChopReadytoCook28b42f.jpg&h=1000&fit=cover",
-                altText: "this is a chicken"
-            }
-        ],
+        photo: {
+            url:
+                "https://images.weserv.nl/?url=https://s3-ap-southeast-1.amazonaws.com/v3-live.image.oddle.me/product/5Combo5ChickenChopReadytoCook28b42f.jpg&h=1000&fit=cover",
+            altText: "this is a chicken",
+        },
+
         title: "5包装鸡扒 Combo 5 Chicken Chop - Ready to Cook",
         description: `
 好易煮 —— 鸡扒来咯！ 
@@ -27,15 +27,16 @@ const products: Product[] = [
 “好易煮”系列是农家平常在家所吃的菜式，省略了现代家庭烹煮过程中繁琐的食材选择、清理、调味腌制的步骤，做到轻松「剪开即煮，快速可上桌」的特点。
 
 我们将腌制好的食材以真空包装来处理，「一包一次」的分量，方便妈妈们收藏、烹煮和品尝。`,
-        price: "RM59.90",
-    }, {
+        price: 59.9,
+    },
+    {
         id: "8861370",
-        photoUrl: [
-            {
-                url: "https://images.weserv.nl/?url=https://s3-ap-southeast-1.amazonaws.com/v3-live.image.oddle.me/product/V21Pumpkinslice800gfb20c2.jpg&amp;h=1000&amp;fit=cover",
-                altText: "this is a pumpkin slice"
-            }
-        ],
+        photo: {
+            url:
+                "https://images.weserv.nl/?url=https://s3-ap-southeast-1.amazonaws.com/v3-live.image.oddle.me/product/V21Pumpkinslice800gfb20c2.jpg&amp;h=1000&amp;fit=cover",
+            altText: "this is a pumpkin slice",
+        },
+
         title: "V21  南瓜块 Pumpkin slice (800g+-)",
         description: `
 南瓜块 Pumpkin slice
@@ -44,42 +45,46 @@ const products: Product[] = [
 • 1 Slice ：800g+-
 
 `,
-        price: "RM3.90",
-    }, {
+        price: 3.9,
+    },
+    {
         id: "8861371",
-        photoUrl: [
-            {
-                url: "https://images.weserv.nl/?url=https://s3-ap-southeast-1.amazonaws.com/v3-live.image.oddle.me/product/V16LimaBeans220ged9a6c.jpg&amp;h=1000&amp;fit=cover",
-                altText: "this is a Lima Beans"
-            }
-        ],
+        photo: {
+            url:
+                "https://images.weserv.nl/?url=https://s3-ap-southeast-1.amazonaws.com/v3-live.image.oddle.me/product/V16LimaBeans220ged9a6c.jpg&amp;h=1000&amp;fit=cover",
+            altText: "this is a Lima Beans",
+        },
+
         title: "V16  棉豆 Lima Beans (220g+-)",
         description: "",
-        price: "RM1.90",
-    }, {
+        price: 1.9,
+    },
+    {
         id: "8861372",
-        photoUrl: [
-            {
-                url: "https://images.weserv.nl/?url=https://s3-ap-southeast-1.amazonaws.com/v3-live.image.oddle.me/product/V1FrenchBeanse929df.jpg&amp;h=1000&amp;fit=cover",
-                altText: "this is a French Beans"
-            }
-        ],
+        photo: {
+            url:
+                "https://images.weserv.nl/?url=https://s3-ap-southeast-1.amazonaws.com/v3-live.image.oddle.me/product/V1FrenchBeanse929df.jpg&amp;h=1000&amp;fit=cover",
+            altText: "this is a French Beans",
+        },
+
         title: "V1   富贵豆 French Beans (250g+-)",
         description: `Bukit Tinggi famous 
 
 富贵豆 French Beans
         
 • 1Pack (250g+-) `,
-        price: "RM3.90",
-    }, {
+        price: 3.9,
+    },
+    {
         id: "8861373",
-        photoUrl: [
-            {
-                url: "https://images.weserv.nl/?url=https://s3-ap-southeast-1.amazonaws.com/v3-live.image.oddle.me/product/Sinceromultifunction6LpressurecookerSPC9001baee74.jpg&w=500&h=500&fit=cover",
-                altText: "this is a cooker"
-            }
-        ],
-        title: "Sincero multifunction 6L pressure cooker SPC-9001 多功能的智能电压力锅",
+        photo: {
+            url:
+                "https://images.weserv.nl/?url=https://s3-ap-southeast-1.amazonaws.com/v3-live.image.oddle.me/product/Sinceromultifunction6LpressurecookerSPC9001baee74.jpg&w=500&h=500&fit=cover",
+            altText: "this is a cooker",
+        },
+
+        title:
+            "Sincero multifunction 6L pressure cooker SPC-9001 多功能的智能电压力锅",
         description: `来自Sincero 的Pressure Cooker 多功能的智能气压锅 ，安全耐用，操作方便，一锅多用又省时！是现代家庭最理想的炊具。它可以用来蒸、煮、炖、焖，烘的功能 。
 
 您可以根据不同的菜肴来监控和调整整个烹饪时间。方便又安全! 
@@ -124,9 +129,8 @@ Easy to Cook Series:
 ⭐ ️ fresh preservation
 ⭐ ️ frozen vacuum packaging, easy to keep
 ⭐ ️ storage period for 3 months`,
-        price: "RM469.90",
-    }
+        price: 469.9,
+    },
+];
 
-]
-
-export default products
+export default products;
